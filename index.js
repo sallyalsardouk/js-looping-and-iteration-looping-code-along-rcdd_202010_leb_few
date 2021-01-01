78fdca2c999ecba7cae876748da7572fc63f83b0
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-01 15:23:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(name,event){
+  let newArray = []
+  for(let i=0;i<name.length;i++){
+    newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!` );
+    
+  }
+  return newArray;
+}
+writeCards(["Lisa","Kaitlin","Jan"],'surprise');
+
+
+
+
+function countDown(nb){
+  while(nb>=0){
+    console.log(nb--);
+    
+  }
+}
+countDown(10);
