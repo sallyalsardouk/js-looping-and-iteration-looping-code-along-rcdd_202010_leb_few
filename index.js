f83d2d3c7633ba98c3a86c15e3f70514e5fa0c6e
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-01 15:14:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 function writeCards(name,event){
+  let newArray = []
   for(let i=0;i<name.length;i++){
     console.log(`Thank you, ${name[i]}for the wonderful ${event} gift!` );
     
   }
-  return name,event;
+  return newArray;
 }
 writeCards(["Lisa","Kaitlin","Jan"],'surprise');
 
 
-function writeCards(names, ocassion) {
-  let newArray = []
-  for (let i = 0; i < names.length; i++) {
 
-    newArray.push(`Thank you, ${names[i]}, for the wonderful ${ocassion} gift!`);
-    // console.log(`Thank you ${names[i]}, for the wonderful ${ocassion} gift!`);
-  }
-  return newArray;
-}
 
 function countDown(nb){
   while(nb>=0){
