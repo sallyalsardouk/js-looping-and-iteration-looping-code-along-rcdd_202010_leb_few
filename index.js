f8f026d6bbf107a3834fe10abe5662b349d14251
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-01 15:01:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name,event){
+  for(let i=0;i<name.length;i++){
+    console.log(`Thank you, ${name[i]}for the wonderful ${event} gift!` );
+    
+  }
+  return name,event;
+}
+writeCards(["Lisa","Kaitlin","Jan"],'surprise');
+
+
+function countDown(nb){
+  while(nb>=0){
+    console.log(nb--);
+    
+  }
+}
+countDown(10);
